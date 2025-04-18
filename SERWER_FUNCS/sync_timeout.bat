0b2c39803887a7dbc3fa04fb90e0a8e51f777cee
CweleINC <cwele.inc@gmail.com>
2025-04-18 17:22:13 +0200

    -04-18   17:22

--- SERWER_FUNCS/sync_timeout.bat
+++ SERWER_FUNCS/sync_timeout.bat
@@ -7,5 +7,5 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
 git commit -m "%mydate%   %mytime%"
 git push
-timeout 600
+timeout 1
 goto start_timeout
